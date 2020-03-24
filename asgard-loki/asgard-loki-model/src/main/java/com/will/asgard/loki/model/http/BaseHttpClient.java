295f64b11553bd341b68c0b4ebd6a8e6020e62ee
maolingwei <maolingwei@xiaomi.com>
2020-03-24 17:22:36 +0800

    feat: http client test

---
+++ asgard-loki/asgard-loki-model/src/main/java/com/will/asgard/loki/model/http/BaseHttpClient.java
@@ -0,0 +1,11 @@
+package com.will.asgard.loki.model.http;
+
+/**
+ * @ClassName BaseHttpClient
+ * @Description TODO
+ * @Auther maolingwei
+ * @Date 2020-03-24 16:52
+ * @Version 1.0
+ **/
+public class BaseHttpClient {
+}
