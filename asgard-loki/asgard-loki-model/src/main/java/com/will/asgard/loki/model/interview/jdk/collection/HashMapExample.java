db87c7cdcdd006b7d49024519de42534f917f135
maolingwei <maolingwei@xiaomi.com>
2020-03-31 17:23:51 +0800

    test: hashmap

---
+++ asgard-loki/asgard-loki-model/src/main/java/com/will/asgard/loki/model/interview/jdk/collection/HashMapExample.java
@@ -0,0 +1,27 @@
+package com.will.asgard.loki.model.interview.jdk.collection;
+
+/**
+ * @ClassName HashMapExample
+ * @Description TODO
+ * @Auther maolingwei
+ * @Date 2020-03-27 16:34
+ * @Version 1.0
+ **/
+public class HashMapExample {
+	private static final int MAXIMUM_CAPACITY = 1 << 30;
+
+	static final int tableSizeFor(int cap) {
+		int n = cap - 1;
+		n |= n >>> 1;
+		n |= n >>> 2;
+		n |= n >>> 4;
+		n |= n >>> 8;
+		n |= n >>> 16;
+		return (n < 0) ? 1 : (n >= MAXIMUM_CAPACITY) ? MAXIMUM_CAPACITY : n + 1;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(tableSizeFor(7));
+	}
+
+}
---
+++ asgard-loki/asgard-loki-model/src/test/java/com/will/asgard/loki/model/interview/jdk/collection/HashMapExampleTest.java
@@ -0,0 +1,40 @@
+package com.will.asgard.loki.model.interview.jdk.collection;
+
+import java.util.Map;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @ClassName HashMapExampleTest
+ * @Description TODO
+ * @Auther maolingwei
+ * @Date 2020-03-27 17:25
+ * @Version 1.0
+ **/
+public class HashMapExampleTest {
+
+	@Test
+	public void testMyNode() {
+
+	}
+
+	private static class MyNode implements Map.Entry {
+
+		@Override
+		public Object getKey() {
+			return null;
+		}
+
+		@Override
+		public Object getValue() {
+			return null;
+		}
+
+		@Override
+		public Object setValue(Object value) {
+			return null;
+		}
+	}
+}
