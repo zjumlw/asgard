2019d45a75fab0470c1c649aea9f5e2022abbf58
zjumlw <maolingwei218@163.com>
2020-04-04 23:25:04 +0800

    test: test hashmap

--- asgard-loki/asgard-loki-model/src/test/java/com/will/asgard/loki/model/interview/jdk/collection/HashMapExampleTest.java
+++ asgard-loki/asgard-loki-model/src/test/java/com/will/asgard/loki/model/interview/jdk/collection/HashMapExampleTest.java
@@ -1,10 +1,9 @@
 package com.will.asgard.loki.model.interview.jdk.collection;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 /**
  * @ClassName HashMapExampleTest
@@ -16,25 +15,70 @@ import static org.junit.Assert.*;
 public class HashMapExampleTest {
 
 	@Test
-	public void testMyNode() {
-
+	public void test() {
+		for (int i = 10; i < 10000000; i *= 10) {
+			doTest(i);
+		}
 	}
 
-	private static class MyNode implements Map.Entry {
+	private void doTest(int mapSize) {
+		Map<Key, Integer> map = new HashMap<>(mapSize);
+		for (int i = 0; i < mapSize; i++) {
+			map.put(Keys.of(i), i);
+		}
 
-		@Override
-		public Object getKey() {
-			return null;
+		long beginTime = System.nanoTime();
+		for (int i = 0; i < mapSize; i++) {
+			map.get(Keys.of(i));
+		}
+		long endTime = System.nanoTime();
+		long total = endTime - beginTime;
+		long average = total / mapSize;
+		System.out.println(mapSize + " : " + average);
+	}
+
+	private static class Keys {
+		public static final int MAX_KEY = 10000000;
+		private static final Key[] KEYS_CACHE = new Key[MAX_KEY];
+
+		static {
+			for (int i = 0; i < MAX_KEY; i++) {
+				KEYS_CACHE[i] = new Key(i);
+			}
+		}
+
+		static Key of(int value) {
+			return KEYS_CACHE[value];
+		}
+	}
+
+	private static class Key implements Comparable<Key> {
+		private final int value;
+
+		public Key(int value) {
+			this.value = value;
 		}
 
 		@Override
-		public Object getValue() {
-			return null;
+		public int compareTo(Key o) {
+			return Integer.compare(this.value, o.value);
 		}
 
 		@Override
-		public Object setValue(Object value) {
-			return null;
+		public boolean equals(Object obj) {
+			if (this == obj) {
+				return true;
+			}
+			if (obj == null || getClass() != obj.getClass()) {
+				return false;
+			}
+			Key key = (Key) obj;
+			return value == ((Key) obj).value;
+		}
+
+		@Override
+		public int hashCode() {
+			return value; // 改变成1为很差的hashcode值
 		}
 	}
 }
