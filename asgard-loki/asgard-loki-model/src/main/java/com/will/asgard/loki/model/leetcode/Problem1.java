95b34985c875a88646ac733686d0d57134ba6467
maolingwei <maolingwei@xiaomi.com>
2020-04-17 15:32:34 +0800

    refactor(leetcode): p1

--- asgard-loki/asgard-loki-model/src/main/java/com/will/asgard/loki/model/leetcode/Problem1.java
+++ asgard-loki/asgard-loki-model/src/main/java/com/will/asgard/loki/model/leetcode/Problem1.java
@@ -20,46 +20,46 @@ import com.will.asgard.common.util.GsonUtil;
  */
 public class Problem1 {
 
-    public int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
-                }
-            }
-        }
-        throw new IllegalArgumentException("No two sum solution");
-    }
+	public int[] twoSum(int[] nums, int target) {
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = i + 1; j < nums.length; j++) {
+				if (nums[i] + nums[j] == target) {
+					return new int[]{i, j};
+				}
+			}
+		}
+		throw new IllegalArgumentException("No two sum solution");
+	}
 
-    /**
-     * faster with HashMap
-     *
-     * @param nums
-     * @param target
-     * @return
-     */
-    public int[] twoSumSolutionMap(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
+	/**
+	 * faster with HashMap
+	 *
+	 * @param nums
+	 * @param target
+	 * @return
+	 */
+	public int[] twoSumSolutionMap(int[] nums, int target) {
+		Map<Integer, Integer> map = new HashMap<>();
 
-        for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
-            if (map.containsKey(complement) && map.get(complement) != i) {
-                return new int[]{i, map.get(complement)};
-            }
-            map.put(nums[i], i);
-        }
+		for (int i = 0; i < nums.length; i++) {
+			int complement = target - nums[i];
+			if (map.containsKey(complement) && map.get(complement) != i) {
+				return new int[]{map.get(complement), i};
+			}
+			map.put(nums[i], i);
+		}
 
-        throw new IllegalArgumentException("No two sum solution");
-    }
+		return new int[2];
+	}
 
 
-    public static void main(String[] args) {
-        int[] nums = new int[]{2, 7, 11, 15};
-        int target = 9;
+	public static void main(String[] args) {
+		int[] nums = new int[]{2, 7, 11, 15};
+		int target = 9;
 
-        int[] result = new Problem1().twoSumSolutionMap(nums, target);
+		int[] result = new Problem1().twoSumSolutionMap(nums, target);
 
-        System.out.println(GsonUtil.toJson(result));
-    }
+		System.out.println(GsonUtil.toJson(result));
+	}
 
 }
