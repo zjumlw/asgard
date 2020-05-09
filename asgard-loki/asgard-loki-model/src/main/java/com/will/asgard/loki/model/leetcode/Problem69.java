e6fadf4c15209e2f55232ba0a5260e264a292594
zjumlw <maolingwei218@163.com>
2020-05-09 18:31:14 +0800

    feat(leetcode): p69

---
+++ asgard-loki/asgard-loki-model/src/main/java/com/will/asgard/loki/model/leetcode/Problem69.java
@@ -0,0 +1,38 @@
+package com.will.asgard.loki.model.leetcode;
+
+/**
+ * @ClassName Problem69
+ * @Description TODO
+ * @Auther maolingwei
+ * @Date 2020-05-09 17:15
+ * @Version 1.0
+ **/
+public class Problem69 {
+
+    public static int mySqrt(int x) {
+        int left = 0;
+        int right = x;
+        int ans = -1;
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            if ((long) mid * mid <= x) {
+                ans = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(mySqrt(0));
+        System.out.println(mySqrt(1));
+        System.out.println(mySqrt(2));
+        System.out.println(mySqrt(3));
+        System.out.println(mySqrt(4));
+        System.out.println(mySqrt(15));
+        System.out.println(mySqrt(16));
+        System.out.println(mySqrt(2147395599));
+    }
+}
