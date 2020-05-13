34184d240f2fdfbade9bc36b15c417058743833b
zjumlw <maolingwei218@163.com>
2020-05-14 01:04:30 +0800

    feat(leetcode): array

---
+++ asgard-loki/asgard-loki-model/src/main/java/com/will/asgard/loki/model/leetcode/Problem136.java
@@ -0,0 +1,42 @@
+package com.will.asgard.loki.model.leetcode;
+
+/**
+ * @ClassName Problem136
+ * @Description
+ * 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+ *
+ * 说明：
+ *
+ * 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+ *
+ * 示例 1:
+ *
+ * 输入: [2,2,1]
+ * 输出: 1
+ * 示例 2:
+ *
+ * 输入: [4,1,2,1,2]
+ * 输出: 4
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/single-number
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @Author maolingwei
+ * @Date 2020-05-14 00:11
+ * @Version 1.0
+ **/
+public class Problem136 {
+
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for (int num : nums) {
+            res ^= num;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 2, 1, 2, 3};
+        System.out.println(new Problem136().singleNumber(nums));
+    }
+}
