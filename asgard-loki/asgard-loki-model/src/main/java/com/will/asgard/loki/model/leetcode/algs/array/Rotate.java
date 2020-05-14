3e691f41a91fd1d56b7de0c3648c1841cd7f1a5c
zjumlw <maolingwei218@163.com>
2020-05-15 00:15:25 +0800

    feat(leetcode): rotate img

--- asgard-loki/asgard-loki-model/src/main/java/com/will/asgard/loki/model/leetcode/algs/array/Rotate.java
+++ asgard-loki/asgard-loki-model/src/main/java/com/will/asgard/loki/model/leetcode/algs/array/Rotate.java
@@ -1,5 +1,7 @@
 package com.will.asgard.loki.model.leetcode.algs.array;
 
+import com.will.asgard.loki.model.leetcode.ArrayUtil;
+
 /**
  * @ClassName Rotate
  * @Description
@@ -49,4 +51,66 @@ package com.will.asgard.loki.model.leetcode.algs.array;
  **/
 public class Rotate {
 
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        if (n <= 1) {
+            return;
+        }
+        for (int i = 0; i <= (n - 2) / 2; i++) {
+            for (int j = i; j <= n - 2 - i; j++) {
+                int currentX = i;
+                int currentY = j;
+                int temp = matrix[currentX][currentY];
+
+                int cnt = 0;
+                while (cnt < 4) {
+                    int nextX = currentY;
+                    int nextY = n - 1 - currentX;
+                    int nextTemp = matrix[nextX][nextY];
+                    matrix[nextX][nextY] = temp;
+
+                    currentX = nextX;
+                    currentY = nextY;
+                    temp = nextTemp;
+                    cnt++;
+                }
+            }
+        }
+    }
+
+    /**
+     * 先转置，再每一行翻转
+     */
+    public void rotateV2(int[][] matrix) {
+        int n = matrix.length;
+
+        // transpose matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int tmp = matrix[j][i];
+                matrix[j][i] = matrix[i][j];
+                matrix[i][j] = tmp;
+            }
+        }
+        // reverse each row
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = tmp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        ArrayUtil.printArray(matrix[0]);
+        ArrayUtil.printArray(matrix[1]);
+        ArrayUtil.printArray(matrix[2]);
+
+        new Rotate().rotate(matrix);
+        ArrayUtil.printArray(matrix[0]);
+        ArrayUtil.printArray(matrix[1]);
+        ArrayUtil.printArray(matrix[2]);
+    }
 }
