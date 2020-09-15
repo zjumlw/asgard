34646d76a8da8854fa7c81ab9e5f12b3c1ee5dce
zjulmw <maolingwei218@163.com>
2020-09-16 01:30:05 +0800

    p226 invertTree

--- asgard-loki/asgard-loki-model/src/main/java/com/will/asgard/loki/model/leetcode/Problem226.java
+++ asgard-loki/asgard-loki-model/src/main/java/com/will/asgard/loki/model/leetcode/Problem226.java
@@ -1,30 +1,50 @@
 package com.will.asgard.loki.model.leetcode;
 
 /**
- * 反转二叉树
+ * 翻转一棵二叉树。
+ *
+ * 示例：
+ *
+ * 输入：
+ *
+ *      4
+ *    /   \
+ *   2     7
+ *  / \   / \
+ * 1   3 6   9
+ * 输出：
+ *
+ *      4
+ *    /   \
+ *   7     2
+ *  / \   / \
+ * 9   6 3   1
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/invert-binary-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class Problem226 {
 
-	public static void main(String[] args) {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
 
-	}
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
 
-	public TreeNode invertTree(TreeNode root) {
-		if (root == null) {
-			return null;
-		}
+    public static void main(String[] args) {
+        String data = "1,2,4,null,null,5,null,null,3,null,null";
+        TreeNode root = TreeNodeUtil.deserialize(data);
+        TreeNodeUtil.printTreeNode(root);
 
-		if (root.left == null && root.right == null) {
-			return root;
-		}
-
-		root.left = invertTree(root.left);
-		root.right = invertTree(root.right);
-
-		TreeNode tmp = root.left;
-		root.left = root.right;
-		root.right = tmp;
-
-		return root;
-	}
+        Problem226 problem226 = new Problem226();
+        problem226.invertTree(root);
+        TreeNodeUtil.printTreeNode(root);
+    }
 }
