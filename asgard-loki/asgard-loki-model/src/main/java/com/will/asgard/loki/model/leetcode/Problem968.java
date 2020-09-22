0720369a3f04f88f62ebf06e5d2abffeb81e8a0f
zjulmw <maolingwei218@163.com>
2020-09-22 23:02:05 +0800

    p968 minCameraCover

---
+++ asgard-loki/asgard-loki-model/src/main/java/com/will/asgard/loki/model/leetcode/Problem968.java
@@ -0,0 +1,74 @@
+package com.will.asgard.loki.model.leetcode;
+
+/**
+ * @Description
+ * 给定一个二叉树，我们在树的节点上安装摄像头。
+ *
+ * 节点上的每个摄影头都可以监视其父对象、自身及其直接子对象。
+ *
+ * 计算监控树的所有节点所需的最小摄像头数量。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ *
+ *
+ * 输入：[0,0,null,0,0]
+ * 输出：1
+ * 解释：如图所示，一台摄像头足以监控所有节点。
+ * 示例 2：
+ *
+ *
+ *
+ * 输入：[0,0,null,0,null,0,null,null,0]
+ * 输出：2
+ * 解释：需要至少两个摄像头来监视树的所有节点。 上图显示了摄像头放置的有效位置之一。
+ *
+ * 提示：
+ *
+ * 给定树的节点数的范围是 [1, 1000]。
+ * 每个节点的值都是 0。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/binary-tree-cameras
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @Author maolingwei
+ * @Date 2020-09-22 9:41 下午
+ * @Version 1.0
+ */
+public class Problem968 {
+
+    public int minCameraCover(TreeNode root) {
+        int[] array = dfs(root);
+
+        return array[1];
+    }
+
+    private int[] dfs(TreeNode root) {
+        if (root == null) {
+            return new int[]{Integer.MAX_VALUE / 2, 0, 0};
+        }
+        int[] leftArray = dfs(root.left);
+        int[] rightArray = dfs(root.right);
+        int[] array = new int[3];
+        array[0] = leftArray[2] + rightArray[2] + 1;
+        array[1] = Math.min(array[0], Math.min(leftArray[0] + rightArray[1], leftArray[1] + rightArray[0]));
+        array[2] = Math.min(array[0], leftArray[1] + rightArray[1]);
+        return array;
+    }
+
+    public static void main(String[] args) {
+        Problem968 problem968 = new Problem968();
+
+        String data = "0,0,0,null,null,0,null,null,null";
+        TreeNode root = TreeNodeUtil.deserialize(data);
+        TreeNodeUtil.printTreeNode(root);
+
+        int num = problem968.minCameraCover(root);
+        System.out.println(num);
+    }
+
+
+}
