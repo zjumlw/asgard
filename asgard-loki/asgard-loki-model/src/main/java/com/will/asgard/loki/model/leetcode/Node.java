609e1d6012cabb90f26ca3c05e8b981244d72d5a
zjulmw <maolingwei218@163.com>
2020-09-29 00:32:28 +0800

    p177 connect

---
+++ asgard-loki/asgard-loki-model/src/main/java/com/will/asgard/loki/model/leetcode/Node.java
@@ -0,0 +1,14 @@
+package com.will.asgard.loki.model.leetcode;
+
+/**
+ * @Description TODO
+ * @Author maolingwei
+ * @Date 2020-09-28 10:15 下午
+ * @Version 1.0
+ */
+public class Node {
+    int val;
+    Node left;
+    Node right;
+    Node next;
+}
---
+++ asgard-loki/asgard-loki-model/src/main/java/com/will/asgard/loki/model/leetcode/Problem117.java
@@ -0,0 +1,116 @@
+package com.will.asgard.loki.model.leetcode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @Description
+ * 给定一个二叉树
+ *
+ * struct Node {
+ *   int val;
+ *   Node *left;
+ *   Node *right;
+ *   Node *next;
+ * }
+ * 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+ *
+ * 初始状态下，所有 next 指针都被设置为 NULL。
+ *
+ *  
+ *
+ * 进阶：
+ *
+ * 你只能使用常量级额外空间。
+ * 使用递归解题也符合要求，本题中递归程序占用的栈空间不算做额外的空间复杂度。
+ *  
+ *
+ * 示例：
+ *
+ *
+ *
+ * 输入：root = [1,2,3,4,5,null,7]
+ * 输出：[1,#,2,3,#,4,5,7,#]
+ * 解释：给定二叉树如图 A 所示，你的函数应该填充它的每个 next 指针，以指向其下一个右侧节点，如图 B 所示。
+ *  
+ *
+ * 提示：
+ *
+ * 树中的节点数小于 6000
+ * -100 <= node.val <= 100
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @Author maolingwei
+ * @Date 2020-09-28 10:13 下午
+ * @Version 1.0
+ */
+public class Problem117 {
+
+    public Node connect(Node root) {
+        if (root == null) {
+            return null;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int n = queue.size();
+            Node last = null;
+            for (int i = 1; i <= n; i++) {
+                Node f = queue.poll();
+                if (f.left != null) {
+                    queue.offer(f.left);
+                }
+                if (f.right != null) {
+                    queue.offer(f.right);
+                }
+                if (i != 1) {
+                    last.next = f;
+                }
+                last = f;
+            }
+        }
+        return root;
+    }
+
+    Node last = null, nextStart = null;
+
+    public Node connectV2(Node root) {
+        if (root == null) {
+            return null;
+        }
+        Node start = root;
+        while (start != null) {
+            last = null;
+            nextStart = null;
+            for (Node p = start; p != null; p = p.next) {
+                if (p.left != null) {
+                    handle(p.left);
+                }
+                if (p.right != null) {
+                    handle(p.right);
+                }
+            }
+            start = nextStart;
+        }
+        return root;
+    }
+
+    public void handle(Node p) {
+        if (last != null) {
+            last.next = p;
+        }
+        if (nextStart == null) {
+            nextStart = p;
+        }
+        last = p;
+    }
+
+    public static void main(String[] args) {
+        for (int i = 1; i < 1; i++) {
+            System.out.println("ok");
+        }
+    }
+}
