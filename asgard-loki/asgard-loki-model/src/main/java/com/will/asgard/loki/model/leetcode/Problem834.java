16cf8c088fe5ce0659a3cdab549fe3b787ce339f
zjulmw <maolingwei218@163.com>
2020-10-10 22:21:00 +0800

    p834 sumOfDistancesInTree

--- asgard-loki/asgard-loki-model/src/main/java/com/will/asgard/loki/model/leetcode/Problem834.java
+++ asgard-loki/asgard-loki-model/src/main/java/com/will/asgard/loki/model/leetcode/Problem834.java
@@ -1,5 +1,8 @@
 package com.will.asgard.loki.model.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @Description
  * 给定一个无向、连通的树。树中有 N 个标记为 0...N-1 的节点以及 N-1 条边 。
@@ -34,9 +37,63 @@ package com.will.asgard.loki.model.leetcode;
  */
 public class Problem834 {
 
+    int[] ans;
+    int[] sz;
+    int[] dp;
+    List<List<Integer>> graph;
+
     public int[] sumOfDistancesInTree(int N, int[][] edges) {
-        int[] ans = new int[0];
+        ans = new int[N];
+        sz = new int[N];
+        dp = new int[N];
+        graph = new ArrayList<List<Integer>>();
+        for (int i = 0; i < N; ++i) {
+            graph.add(new ArrayList<Integer>());
+        }
+        for (int[] edge: edges) {
+            int u = edge[0], v = edge[1];
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+        dfs(0, -1);
+        dfs2(0, -1);
         return ans;
+    }
+
+    public void dfs(int u, int f) {
+        sz[u] = 1;
+        dp[u] = 0;
+        for (int v: graph.get(u)) {
+            if (v == f) {
+                continue;
+            }
+            dfs(v, u);
+            dp[u] += dp[v] + sz[v];
+            sz[u] += sz[v];
+        }
+    }
+
+    public void dfs2(int u, int f) {
+        ans[u] = dp[u];
+        for (int v: graph.get(u)) {
+            if (v == f) {
+                continue;
+            }
+            int pu = dp[u], pv = dp[v];
+            int su = sz[u], sv = sz[v];
+
+            dp[u] -= dp[v] + sz[v];
+            sz[u] -= sz[v];
+            dp[v] += dp[u] + sz[u];
+            sz[v] += sz[u];
+
+            dfs2(v, u);
+
+            dp[u] = pu;
+            dp[v] = pv;
+            sz[u] = su;
+            sz[v] = sv;
+        }
     }
 
     public static void main(String[] args) {
